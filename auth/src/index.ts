39b3a4dce57904ede28747dc2a79573c0571c27f
Abdul Mannan <a.mannan0607@gmail.com>
2024-10-12 03:30:13 +0500

    cookie && jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
 import express from "express";
 import "express-async-errors";
 import mongoose from "mongoose";
@@ -10,7 +11,14 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,6 +32,10 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
+    if(!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET must be defined");
+    }
+
     try {
         await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
         console.log("Connected to: " + mongoose.connection.host);
