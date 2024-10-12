4287ab1d4c21493dc0c2282e3a108f3b6024cbb4
Abdul Mannan <a.mannan0607@gmail.com>
2024-10-13 04:45:16 +0500

    section 9: auth configuration

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req: Request, res: Response) => {
-    res.send("Hi there!");
-})
+  req.session = null;
+  res.send({});
+});
 
 export { router as signoutRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,39 +1,44 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { BadRequestError } from "../errors/bad-request-error";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { validateRequest } from "../middlewares/validate-request";
 import { User } from "../models/user";
 const router = express.Router();
 
-router.post("/api/users/signup", [
+router.post(
+  "/api/users/signup",
+  [
     body("email").isEmail().withMessage("Email must be valid"),
-    body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters")
-], async (req: Request, res: Response): Promise<any> => {
-    
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-    
-    const {email, password} = req.body;
-    const existingUser = await User.findOne({email});
-    if(existingUser) {
-        throw new BadRequestError("Email is already registered");
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response): Promise<any> => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError("Email is already registered");
     }
 
-    const user = User.build({email, password});
+    const user = User.build({ email, password });
     await user.save();
 
-    const userJwt = jwt.sign({
+    const userJwt = jwt.sign(
+      {
         id: user.id,
-        email: user.email
-    }, process.env.JWT_SECRET!);
+        email: user.email,
+      },
+      process.env.JWT_SECRET!
+    );
     req.session = {
-        jwt: userJwt
-    }
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
-})
+  }
+);
 
 export { router as signupRouter };
